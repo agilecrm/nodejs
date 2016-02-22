c7618f03afee59dbaf86ba9dbb78268a146f899f
graut <ghanshyam.agile@gmail.com>
2016-02-22 12:30:32 +0530

    Deal methods added
    Deal Operation added

--- agilecrm/agilecrm.js
+++ agilecrm/agilecrm.js
@@ -253,4 +253,187 @@ ContactAPI.prototype.deleteContact = function deleteContact(contactId, success, 
     }
 };
 
+ContactAPI.prototype.createDeal = function createDeal(opportunity, success, failure) {
+    var options = this.getOptions();
+    options.path = '/dev/api/opportunity';
+    options.method = 'POST';
+    options.headers['Content-Type'] = 'application/json';
+
+    var post = https.request(options, function(resp) {
+        resp.setEncoding('utf8');
+        var body = "";
+        resp.on('data', function(data) {
+            body += data;
+        });
+        resp.on('end', function() {
+            if (success) {
+                try {
+                    var opportunity = JSON.parse(body);
+                    success(opportunity);
+                } catch (ex) {
+                    failure(ex);
+                }
+            }
+        });
+        resp.on('error', function(e) {
+            if (failure) {
+                failure(e);
+            }
+        });
+    });
+
+    try {
+        var data = JSON.stringify(opportunity);
+        post.write(data);
+        post.end();
+    } catch (ex) {
+        failure(ex);
+    }
+};
+
+ContactAPI.prototype.updateDeal = function updateDeal(opportunity, success, failure) {
+    var options = this.getOptions();
+    options.path = '/dev/api/opportunity/partial-update';
+    options.method = 'PUT';
+    options.headers['Content-Type'] = 'application/json';
+
+    var put = https.request(options, function(resp) {
+        resp.setEncoding('utf8');
+        var body = "";
+        resp.on('data', function(data) {
+            body += data;
+        });
+        resp.on('end', function() {
+            if (success) {
+                try {
+                    var opportunity = JSON.parse(body);
+                    success(opportunity);
+                } catch (ex) {
+                    failure(ex);
+                }
+            }
+        });
+        resp.on('error', function(e) {
+            if (failure) {
+                failure(e);
+            }
+        });
+    });
+
+    try {
+        var data = JSON.stringify(opportunity);
+        put.write(data);
+        put.end();
+    } catch (ex) {
+        failure(ex);
+    }
+};
+
+ContactAPI.prototype.getDealById = function getDealById(dealId, success, failure) {
+    var options = this.getOptions();
+    options.path = '/dev/api/opportunity/' + dealId;
+
+    https.get(options, function (resp) {
+        var body = "";
+        resp.on('data', function(data) {
+            body += data;
+        });
+        resp.on('end', function() {
+            if (success) {
+                if (body) {
+                    try {
+                        var opportunity = JSON.parse(body);
+                        success(opportunity);
+                    } catch (ex) {
+                        failure(ex);
+                    }
+                }
+            } else {
+                success({});
+            }
+        })
+        resp.on('error', function(e) {
+            if (failure) {
+                failure(e);
+            }
+        });
+    }).on("error", function(e){
+        if (failure) {
+            failure(e);
+        }
+    });
+};
+
+ContactAPI.prototype.getDealByContactId = function getDealByContactId(contactId, success, failure) {
+    var options = this.getOptions();
+    options.path = '/dev/api/contacts/' + contactId + 'deals';
+
+    https.get(options, function (resp) {
+        var body = "";
+        resp.on('data', function(data) {
+            body += data;
+        });
+        resp.on('end', function() {
+            if (success) {
+                if (body) {
+                    try {
+                        var opportunity = JSON.parse(body);
+                        success(opportunity);
+                    } catch (ex) {
+                        failure(ex);
+                    }
+                }
+            } else {
+                success({});
+            }
+        })
+        resp.on('error', function(e) {
+            if (failure) {
+                failure(e);
+            }
+        });
+    }).on("error", function(e){
+        if (failure) {
+            failure(e);
+        }
+    });
+};
+
+
+ContactAPI.prototype.deleteDealById = function deleteDealById(dealId, success, failure) {
+    var options = this.getOptions();
+    options.path = '/dev/api/opportunity/' + dealId;
+    options.method = 'DELETE';
+	options.headers['Content-Type'] = 'application/json';
+
+    var del = https.request(options, function(resp) {
+        resp.setEncoding('utf8');
+        var body = "";
+        resp.on('data', function(data) {
+            body += data;
+        });
+        resp.on('end', function() {
+            if (success) {
+                try {
+                    
+                    success(body);
+                } catch (ex) {
+                    failure(ex);
+                }
+            }
+        });
+        resp.on('error', function(e) {
+            if (failure) {
+                failure(e);
+            }
+        });
+    });
+
+    try {
+        del.end();
+    } catch (ex) {
+        failure(ex);
+    }
+};
+
 module.exports = AgileCRMManager;
